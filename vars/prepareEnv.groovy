7dae7e3847b2384e9243c9b6e6aa9153bc916878
Guy Bochner <67061691+guy-bo@users.noreply.github.com>
2025-02-15 09:34:41 +0200

    Update prepareEnv.groovy
    initial function

--- vars/prepareEnv.groovy
+++ vars/prepareEnv.groovy
@@ -1,1 +1,24 @@
+def verify_parametr(parameters, param){
+    if (!parameters.containsKey(name)){
+        throw new Exception("Missing parameter: ${param}")
+    }
+}
 
+def get(script, parameters){
+
+}
+
+def call(Map parameters = [:]){
+    verify_parametr(parameters, 'script')
+    verify_parametr(parameters, 'action')
+    switch(parameters.action){
+        case 'get':
+            get(script, parameters)
+            break
+        default:
+            // unsupported action
+            break
+    }
+}
+
+return this
